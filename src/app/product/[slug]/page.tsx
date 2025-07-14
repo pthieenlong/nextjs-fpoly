5b9defe2038005d5031efe4bf06c988a42b371b1
pthieenlong <p.thieenlong.304@gmail.com>
2025-07-14 16:15:41 +0700

    fix product detail

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,35 +1,18 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Breadcrumbs, Link, Typography } from "@mui/material";
 import { API_ROUTE } from "@/__const/const";
 import ProductLayout from "../__layout/Product.layout";
 import ContentLayout from "../__layout/Content.layout";
-import { useParams } from "next/navigation";
-function ProductDetail() {
-  const params = useParams();
-  const slug = params.slug;
-
-  const [product, setProduct] = useState();
-  const [productName, setProductName] = useState("");
-  useEffect(() => {
-    fetch(`${API_ROUTE}/product/${slug}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        if (data.success) {
-          setProduct(data.data);
-          setProductName(data.data.name);
-        }
-        return data;
-      })
-      .catch((error: any) => {
-        return error;
-      });
-  }, [slug]);
-
+async function ProductDetail({ params }: { params: { slug: string } }) {
+  const resolvedParams = await params;
+  const productFetching = await fetch(
+    `${API_ROUTE}/product/${resolvedParams.slug}`
+  );
+  const productResponse = await productFetching.json();
+  const product = productResponse.data;
   return (
     <section className="max-w-[1280px] m-auto flex flex-col gap-6">
+      {/* Breadcrumbs */}
       <Breadcrumbs aria-label="breadcrumb">
         <Link underline="hover" color="inherit" href="/">
           Homepage
@@ -37,10 +20,9 @@ function ProductDetail() {
         <Link underline="hover" color="inherit" href="/product">
           All products
         </Link>
-        <Typography>{productName}</Typography>
+        <Typography>{product.name}</Typography>
       </Breadcrumbs>
       <ProductLayout product={product} />
-
       <ContentLayout />
     </section>
   );
--- src/app/product/__layout/Product.layout.tsx
+++ src/app/product/__layout/Product.layout.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import { Rating } from "@smastrom/react-rating";
 import {
